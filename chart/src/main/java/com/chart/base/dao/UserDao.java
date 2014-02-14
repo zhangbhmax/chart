c15dbd5dffb74e177955375689b54f9be0b0feb7
zhangbhmax <m13655199236@163.com>
2014-02-14 17:21:33 +0800

    用户管理

--- chart/src/main/java/com/chart/base/dao/UserDao.java
+++ chart/src/main/java/com/chart/base/dao/UserDao.java
@@ -1,7 +1,26 @@
 package com.chart.base.dao;
 
 import com.chart.base.bean.User;
+import com.chart.common.bean.Page;
 
 public interface UserDao {
+	/**
+	 * 添加用户
+	 * @param user
+	 */
 	void addUser(User user);
+	
+	/**
+	 * 用户分页查询
+	 * @param user
+	 * @return
+	 */
+	Page<User> findUser(User user);
+	
+	/**
+	 * 根据用户名查询用户信息
+	 * @param userName
+	 * @return
+	 */
+	User getUser(String userName);
 }
--- chart/src/main/java/com/chart/base/dao/impl/UserDaoImpl.java
+++ chart/src/main/java/com/chart/base/dao/impl/UserDaoImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Repository;
 
 import com.chart.base.bean.User;
 import com.chart.base.dao.UserDao;
+import com.chart.common.bean.Page;
 import com.chart.common.dao.BaseDao;
 
 @Repository
@@ -11,6 +12,18 @@ public class UserDaoImpl extends BaseDao implements UserDao {
 
 	public void addUser(User user) {
 		getSqlMapClientTemplate().insert("user.addUser", user);
+	}
+
+	@Override
+	public Page<User> findUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUser(String userName) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
